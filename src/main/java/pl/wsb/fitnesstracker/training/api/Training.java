3c56baa2d22f087c91cb10556dac3b3000955d1a
Your Name <your.email@example.com>
2026-03-29 18:47:48 +0200

    laboratorium2

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,27 +1,45 @@
 package pl.wsb.fitnesstracker.training.api;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
 
 import java.util.Date;
 
 @Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "trainings")
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "start_time", nullable = false)
     private Date startTime;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "end_time", nullable = true)
     private Date endTime;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "activity_type", nullable = false)
     private ActivityType activityType;
 
+    @Column(name = "distance", nullable = false)
     private double distance;
 
-    private double averageSpeed;
+    @Column(name = "average_speed", nullable = true)
+    private Double averageSpeed;
 
     public Training(
             final User user,
@@ -29,7 +47,8 @@ public class Training {
             final Date endTime,
             final ActivityType activityType,
             final double distance,
-            final double averageSpeed) {
+            final Double averageSpeed
+    ) {
         this.user = user;
         this.startTime = startTime;
         this.endTime = endTime;
